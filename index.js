3edf024f93784919242dbdf7b04ef6c6d782da8e
pavangowthamkrishna <pavan.pedakala@gmail.com>
2022-11-15 12:51:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+l=799
+s=199
+
+d=(l-s)/l*100
+console.log(d)
+displydiscountpercent=math.round(discountt)
---
+++ js.js
@@ -0,0 +1,10 @@
+var username="gowtham"
+var dateOFBirth="09-09-1999"
+var emailid="powernippu@gmail.com"
+var qualification="wastefellow"
+console.log(`
+    studentname:${username}
+    dob:${dateOFBirth}
+    eid:${emailid}
+    qua:${qualification}
+`);
